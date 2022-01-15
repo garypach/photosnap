036af7a396c30c35484df34f9d11c74e47cdf39f
Gary <58466698+garypach@users.noreply.github.com>
2022-01-14 16:19:58 -0800

    responsive features page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
         // => @media (min-width: 768px) { ... }
         
         'lg': '1024px',
-        // => @media (min-width: 769px) { ... }
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1205px',
+
+        'xxl': '1440px',
       },
     },
     plugins: [],
