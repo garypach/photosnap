37ba613dd5b37030afe8bdef97ad990eb3b43590
Gary <58466698+garypach@users.noreply.github.com>
2022-01-15 13:43:30 -0800

    responsive home page

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,31 @@
-$(function() {
-    $('#hero-text-invite').hover(function() {
-      $('#stories-hero-overlay').css('background-color', 'yellow');
-    }, function() {
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+$(function () {
+  $("#hero-text-invite").hover(
+    function () {
+      $("#stories-hero-overlay").css("background-color", "yellow");
+    },
+    function () {
       // on mouseout, reset the background colour
-      $('#stories-hero-overlay').css('background-color', 'black');
-    });
-  });
+      $("#stories-hero-overlay").css("background-color", "black");
+    }
+  );
+});
+
