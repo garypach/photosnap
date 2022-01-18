b0a560142fe364b2cc4e0316014665f578d81c4a
Gary <58466698+garypach@users.noreply.github.com>
2022-01-17 16:07:22 -0800

    update header update footer and add toggle on price page

--- src/js/app.js
+++ src/js/app.js
@@ -29,3 +29,37 @@ $(function () {
   );
 });
 
+function switchPrice() {
+  let basic = document.getElementsByClassName('cost')[0];
+  let basicTime = document.getElementsByClassName('time-frame')[0];
+  let pro = document.getElementsByClassName('cost')[1];
+  let proTime = document.getElementsByClassName('time-frame')[1];
+  let business = document.getElementsByClassName('cost')[2];
+  let businessTime = document.getElementsByClassName('time-frame')[2];
+  if (basic.innerHTML === "$19.00") {
+    basic.innerHTML = "$190.00";
+    basicTime.innerHTML = "per year";
+
+    pro.innerHTML = "$390.00";
+    proTime.innerHTML = "per year";
+
+    business.innerHTML = "$990.00";
+    businessTime.innerHTML = "per year";
+
+    document.getElementsByClassName('yearly')[0].style.opacity = "1"
+    document.getElementsByClassName('monthly')[0].style.opacity = ".5"
+
+  } else {
+    basic.innerHTML = "$19.00";
+    basicTime.innerHTML = "per month"
+    pro.innerHTML = "$39.00";
+    proTime.innerHTML = "per month"
+    business.innerHTML = "$99.00";
+    businessTime.innerHTML = "per month"
+    document.getElementsByClassName('yearly')[0].style.opacity = ".5"
+    document.getElementsByClassName('monthly')[0].style.opacity = "1"
+
+  }
+
+ 
+} 
